ec70fa3ca505233048e2cc2f5bbf6c8c8969b618
byt3-m3 <cbaxtertech@gmail.com>
2019-01-06 20:15:46 +0300

    Added command

--- dockerfiles/commands.sh
+++ dockerfiles/commands.sh
@@ -6,3 +6,7 @@ docker build -f dockerfiles/dev.Dockerfile -t cbaxter1988/service_confparser:dev
 
 # Builds Test environment for service, Source files are copied to the container and executed.
 docker build -f dockerfiles/build.Dockerfile -t cbaxter1988/service_confparser:test .
+
+
+# Runs the docker service
+ docker run --rm -p 50052:50052 cbaxter1988/service_confparser:test
